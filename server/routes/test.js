b8fa4276bc626e1b5d867444cc863fe27b41069e
Chun Hei Law <brightttton@Chuns-MacBook-Pro.local>
2021-05-08 19:26:13 +0800

    try

--- server/routes/test.js
+++ server/routes/test.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function (req, res, next) {
+router.get("/test", function (req, res) {
     res.send("12345678 this message is sent from the server");
     //
 });
