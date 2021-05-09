46c5efe7e12d399255ce667859095103ddffc74b
Chun Hei Law <brightttton@Chuns-MacBook-Pro.local>
2021-05-10 07:00:23 +0800

    made passport

--- server/session/passport.js
+++ server/session/passport.js
@@ -1,3 +1,53 @@
 const localstrategy = require('passport-local').Strategy
 const bcrypt =require('bcrypt')
-const user = require('../datamodel/user')
+const User = require('../datamodel/user')
+module.exports = function(passport){
+    passport.use('register',  new localstrategy(
+        function(req, username, password, done){
+            User.findOne({'username':username}).then(user =>{
+                if(password.length <4){
+                    return done(null, false, req.flash('message', 'password must be at least 4 characters'));
+                }
+                if(user){
+                    return done(null, false, req.flash('message', 'username existed'));
+                }else{
+                    const newUser = new User({
+                        username,
+                        password
+                    });
+                    bcrpyt.genSalt(10,(err,salt)=>{
+                        bcrpyt.hash(newUser.password, salt, (err,hash)=>{
+                            newUser.password = hash; 
+                        });    
+                    });
+                    newUser.save(function(err){
+                        if(error) throw err;
+                        return done(null, newUser);
+                    });
+                }
+            });
+        }  
+    ));
+    passport.use('login', new localstrategy(
+        function(req, username, password, done){
+            User.findOne({'username': username}).then(user =>{
+                if(!user) return done(null, false, req.flash('message','This username is not registered'));
+                bcrypt.compare(password, user.passowrd, (err, isMatch)=>{
+                    if(error) throw err;
+                    if(!isMatch) return done(null, false, req.flash('message', 'incorrect password'));
+                    else return done(null, user);
+                });
+            });
+        }
+    ));
+
+
+    passport.serializeUser(function(user, done){
+        done(null,user.id);
+    });
+    passport.deserializeUser(function(id,done){
+        User.findbyId(id,function(err,user){
+            done(err,user);
+        });
+    });
+}
