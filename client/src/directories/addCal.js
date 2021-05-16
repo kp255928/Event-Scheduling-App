2f29b32ce675127a6d660b50bc00dc39aac29168
Ching-Ting Lu <chingtinglu@Ching-Tings-MacBook-Pro.local>
2021-05-17 01:39:22 +0800

    More pages setted up

---
+++ client/src/directories/addCal.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './home.css';
+
+const AddCalendar = () => {
+    return(
+        <div classname="homeText">
+        <h1 className="greet">Add Calendar here!</h1>
+    </div>
+    );
+}
+export default AddCalendar;
