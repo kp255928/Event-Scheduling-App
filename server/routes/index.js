98b7901a1fef7070d720092e5479f778ff207621
Chun Hei Law <lawchunhei88@hotmail.com>
2021-05-30 18:38:51 +0800

    again4

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.get('/register',function(req, res){
     res.render('register.ejs')
 });
-router.post('/register', (req,res) =>{
+router.post('/register', (req,res, next) =>{
   passport.authenticate('register',{
     successRedirect: '/login',
     failureRedirect:'/register',
