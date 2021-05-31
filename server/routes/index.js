bc508b7b4f0c2637bc79b27649aba6479b549c8a
Chun Hei Law <lawchunhei88@hotmail.com>
2021-05-31 16:19:39 +0800

    fuck my life

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,25 +2,57 @@ const express = require('express')
 const passport = require('passport')
 const Event = require('../datamodel/events')
 const User = require('../datamodel/user')
+const bcrypt = require('bcryptjs')
 const router = express.Router();
 // login and re
 // router.set('view-engine', 'ejs');
-
+/*
 router.get('/register',function(req, res){
     res.render('register.ejs')
 });
-
-router.post('/register', (req,res, next) =>{
-  passport.authenticate('register',{
+router.post('/register', 
+  passport.authenticate('local',{
     successRedirect: '/login',
     failureRedirect:'/register',
     failureFlash: true
   })
+);
+*/
+router.post('/register', (req,res) =>{
+  var username = req.body.username;
+  var password = req.body.password;
+  if (password.length<4) return res.status(400).json('password mush be larger than 4')
+  User.findOne({'username':username}).then(user =>{
+    if(user) return res.status(400).json('username taken')
+    else{
+      const newUser = new User({
+        username,
+        password
+      });
+      bcrypt.genSalt(10,(err,salt)=>{
+        bcrypt.hash(newUser.password, salt, (err,hash)=>{
+          if(err) throw errr;
+          newUser.password = hash;
+          newUser.save().then(user=>{
+            req.flash('success','signuped');
+            res.redirect('/login')
+          })
+          .catch(err=>concole.log("failed"));
+        });    
+      });
+    }
+  });
 });
-
-
-
-
+router.get('/login', function(req, res){
+  res.render('login.ejs')
+});
+router.post('/login', (req,res)=>{
+  passport.authenticate('login', {
+  //successRedirect: '/',
+  failureRedirect:'/login',
+  failureFlash: true
+})
+});
 router.delete('/logout', (req,res) =>{
     req.logOut()
     res.redirect('/login')
--- server/session/passport.js
+++ server/session/passport.js
@@ -1,10 +1,37 @@
 const localstrategy = require('passport-local').Strategy
 const bcrypt =require('bcrypt')
 const User = require('../datamodel/user')
-const register = require('./register')
 module.exports = function(passport){
-    register(passport);
-    
+    passport.use('register',  new localstrategy(
+        {usernameField : 'username'},
+        {passwordField : 'password'},
+        {passReqToCallback: true},
+        function(req, username, password, done){
+            User.findOne({'username':username}),function(err, user){
+                if(err)
+                    return done(null, false,req.flash('message','failed'))
+                if(password.length <4){
+                    return done(null, false, req.flash('message', 'password must be at least 4 characters'));
+                }
+                if(user){
+                    return done(null, false, req.flash('message', 'username existed'));
+                }else{
+                    const newUser = new User();
+                    newUser.username = username;
+                    newUser.password = password;
+                    bcrpyt.genSalt(10,(err,salt)=>{
+                        bcrpyt.hash(newUser.password, salt, (err,hash)=>{
+                            newUser.password = hash; 
+                        });    
+                    });
+                    newUser.save(function(err){
+                        if(error) throw err;
+                        return done(null, newUser);
+                    });
+                }
+            }
+        }  
+    ));
     passport.use('login', new localstrategy(
         {passReqToCallback: true},
         function(req, username, password, done){
